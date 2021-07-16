228ff1883398efdcc900097c4923f22c12ba17d2
Positive2022 <70850451+Positive2022@users.noreply.github.com>
2021-07-16 14:20:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const Mute = require('./functions/Mute.js')
+const Unmute = require('./functions/Unmute.js')
+const Kick = require('./functions/Kick.js')
+const Ban = require('./functions/Ban.js')
+const Unban = require('./functions/Unban.js')
+module.exports = {
+    Mute,
+    Kick,
+    Ban,
+    Unban,
+    Unmute
+}
